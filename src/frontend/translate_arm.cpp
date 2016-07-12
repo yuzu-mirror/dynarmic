f85b86486b123a86200197db6492666a1619e972
MerryMage <MerryMage@users.noreply.github.com>
2016-07-12 09:04:47 +0100

    frontend/TranslateArm: Just interpret all ARM instructions

--- src/frontend/translate_arm.cpp
+++ src/frontend/translate_arm.cpp
@@ -7,14 +7,18 @@
 #include "common/assert.h"
 #include "frontend/arm_types.h"
 #include "frontend/ir/ir.h"
+#include "frontend/ir_emitter.h"
 #include "frontend/translate.h"
 
 namespace Dynarmic {
 namespace Arm {
 
 IR::Block TranslateArm(LocationDescriptor descriptor, MemoryRead32FuncType memory_read_32) {
-    ASSERT_MSG(0, "Unimplemented");
-    return IR::Block(descriptor);
+    // Just interpret everything.
+    IREmitter ir{descriptor};
+    ir.SetTerm(IR::Term::Interpret{ir.current_location});
+    ir.block.cycle_count++;
+    return ir.block;
 }
 
 } // namespace Arm
