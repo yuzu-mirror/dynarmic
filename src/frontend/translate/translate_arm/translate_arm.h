939bb5c0cb376f2b60ce219b2d1e9d2a32e5246f
MerryMage <MerryMage@users.noreply.github.com>
2016-08-07 20:08:31 +0100

    TranslateArm: Implement NOP

--- src/frontend/translate/translate_arm/translate_arm.h
+++ src/frontend/translate/translate_arm/translate_arm.h
@@ -194,7 +194,7 @@ struct ArmTranslatorVisitor final {
 
     // Miscellaneous instructions
     bool arm_CLZ(Cond cond, Reg d, Reg m) { return InterpretThisInstruction(); }
-    bool arm_NOP() { return InterpretThisInstruction(); }
+    bool arm_NOP() { return true; }
     bool arm_SEL(Cond cond, Reg n, Reg d, Reg m) { return InterpretThisInstruction(); }
 
     // Unsigned sum of absolute difference functions
