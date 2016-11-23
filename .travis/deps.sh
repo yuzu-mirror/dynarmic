4c817b9bd5de9ceefd8f56218fc10fc61a577f60
MerryMage <MerryMage@users.noreply.github.com>
2016-11-23 18:23:17 +0000

    travis: No longer necessary to install latest cmake from homebrew on macOS

--- .travis/deps.sh
+++ .travis/deps.sh
@@ -12,7 +12,4 @@ if [ "$TRAVIS_OS_NAME" = "linux" -o -z "$TRAVIS_OS_NAME" ]; then
     mkdir -p $HOME/.local
     curl -L https://cmake.org/files/v3.4/cmake-3.4.1-Linux-i386.tar.gz \
         | tar -xz -C $HOME/.local --strip-components=1
-elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    brew update > /dev/null # silence the very verbose output
-    brew install cmake
 fi
