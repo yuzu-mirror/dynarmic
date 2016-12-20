02b2ab7581f827a6cc3fd21a2c240f6b23202403
MerryMage <MerryMage@users.noreply.github.com>
2016-12-20 22:07:51 +0000

    emit_x64: Pass tmp to ExtractMostSignificantBitFromPackedBytes in EmitPackedAddU8

--- src/backend_x64/emit_x64.cpp
+++ src/backend_x64/emit_x64.cpp
@@ -1420,7 +1420,7 @@ void EmitX64::EmitPackedAddU8(IR::Block& block, IR::Inst* inst) {
     }
     code->xor_(result, reg_a);
     if (ge_inst) {
-        ExtractMostSignificantBitFromPackedBytes(cpu_info, code, reg_alloc, reg_ge);
+        ExtractMostSignificantBitFromPackedBytes(cpu_info, code, reg_alloc, reg_ge, tmp);
     }
 }
 
