e197b10b96b9f7226dfd246d73e34478df7de1b7
MerryMage <MerryMage@users.noreply.github.com>
2017-02-16 19:30:56 +0000

    common: Introduce utility function VisitVariant
    VisitVariant allows one to use a generic lambda to visit a boost::variant.
    This is necessary because boost::visit_variant requires the visitor type
    to provide a return type.

---
+++ src/common/variant_util.h
@@ -0,0 +1,33 @@
+/* This file is part of the dynarmic project.
+ * Copyright (c) 2016 MerryMage
+ * This software may be used and distributed according to the terms of the GNU
+ * General Public License version 2 or any later version.
+ */
+
+#pragma once
+
+#include <boost/variant.hpp>
+
+namespace Dynarmic {
+namespace Common {
+
+namespace detail {
+
+template <typename ReturnT, typename Lambda>
+struct VariantVisitor : boost::static_visitor<ReturnT>, Lambda {
+   VariantVisitor(Lambda&& lambda)
+       : Lambda(std::move(lambda))
+   {}
+
+   using Lambda::operator();
+};
+
+} // namespace detail
+
+template<typename ReturnT, typename Variant, typename Lambda>
+inline ReturnT VisitVariant(Variant&& variant, Lambda&& lambda) {
+    return boost::apply_visitor(detail::VariantVisitor<ReturnT, Lambda>(std::move(lambda)), variant);
+}
+
+} // namespace Common
+} // namespace Dynarmic
