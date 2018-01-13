21fe61eac6bf74d90ff8329e12bbfb2b8fb2b5ff
MerryMage <MerryMage@users.noreply.github.com>
2018-01-13 18:02:19 +0000

    A64/data_processing_pcrel: bug: ADR{,P} instructions sign extend their immediate

--- src/frontend/A64/translate/impl/data_processing_pcrel.cpp
+++ src/frontend/A64/translate/impl/data_processing_pcrel.cpp
@@ -10,14 +10,14 @@ namespace Dynarmic {
 namespace A64 {
 
 bool TranslatorVisitor::ADR(Imm<2> immlo, Imm<19> immhi, Reg Rd) {
-    u64 imm = concatenate(immhi, immlo).ZeroExtend<u64>();
+    u64 imm = concatenate(immhi, immlo).SignExtend<u64>();
     u64 base = ir.PC();
     X(64, Rd, ir.Imm64(base + imm));
     return true;
 }
 
 bool TranslatorVisitor::ADRP(Imm<2> immlo, Imm<19> immhi, Reg Rd) {
-    u64 imm = concatenate(immhi, immlo).ZeroExtend<u64>() << 12;
+    u64 imm = concatenate(immhi, immlo).SignExtend<u64>() << 12;
     u64 base = ir.PC() & ~u64(0xFFF);
     X(64, Rd, ir.Imm64(base + imm));
     return true;
