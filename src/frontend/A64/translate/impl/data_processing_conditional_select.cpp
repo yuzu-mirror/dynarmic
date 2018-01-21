a8a65beb2be31413e95262f51161221e310e1043
Lioncash <mathew1800@gmail.com>
2018-01-21 14:24:44 -0500

    data_processsing_conditional_select: Implement CSINC, CSINV and CSNEG

--- src/frontend/A64/translate/impl/data_processing_conditional_select.cpp
+++ src/frontend/A64/translate/impl/data_processing_conditional_select.cpp
@@ -22,5 +22,41 @@ bool TranslatorVisitor::CSEL(bool sf, Reg Rm, Cond cond, Reg Rn, Reg Rd) {
     return true;
 }
 
+bool TranslatorVisitor::CSINC(bool sf, Reg Rm, Cond cond, Reg Rn, Reg Rd) {
+    const size_t datasize = sf ? 64 : 32;
+
+    const IR::U32U64 operand1 = X(datasize, Rn);
+    const IR::U32U64 operand2 = X(datasize, Rm);
+
+    const IR::U32U64 result = ir.ConditionalSelect(cond, operand1, ir.Add(operand2, I(datasize, 1)));
+
+    X(datasize, Rd, result);
+    return true;
+}
+
+bool TranslatorVisitor::CSINV(bool sf, Reg Rm, Cond cond, Reg Rn, Reg Rd) {
+    const size_t datasize = sf ? 64 : 32;
+
+    const IR::U32U64 operand1 = X(datasize, Rn);
+    const IR::U32U64 operand2 = X(datasize, Rm);
+
+    const IR::U32U64 result = ir.ConditionalSelect(cond, operand1, ir.Not(operand2));
+
+    X(datasize, Rd, result);
+    return true;
+}
+
+bool TranslatorVisitor::CSNEG(bool sf, Reg Rm, Cond cond, Reg Rn, Reg Rd) {
+    const size_t datasize = sf ? 64 : 32;
+
+    const IR::U32U64 operand1 = X(datasize, Rn);
+    const IR::U32U64 operand2 = X(datasize, Rm);
+
+    const IR::U32U64 result = ir.ConditionalSelect(cond, operand1, ir.Add(ir.Not(operand2), I(datasize, 1)));
+
+    X(datasize, Rd, result);
+    return true;
+}
+
 } // namespace A64
 } // namespace Dynarmic
