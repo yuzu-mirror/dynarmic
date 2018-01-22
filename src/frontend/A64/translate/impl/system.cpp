ae5dbcbed6d279351f3479c5534c612f0f2624bd
Lioncash <mathew1800@gmail.com>
2018-01-21 19:50:31 -0500

    A64: Implement HINT, NOP, YIELD, WFE, WFI, SEV, and SEVL
    Truly the most difficult A64 instructions to implement.

---
+++ src/frontend/A64/translate/impl/system.cpp
@@ -0,0 +1,41 @@
+/* This file is part of the dynarmic project.
+ * Copyright (c) 2018 MerryMage
+ * This software may be used and distributed according to the terms of the GNU
+ * General Public License version 2 or any later version.
+ */
+
+#include "frontend/A64/translate/impl/impl.h"
+
+namespace Dynarmic {
+namespace A64 {
+
+bool TranslatorVisitor::HINT([[maybe_unused]] Imm<4> CRm, [[maybe_unused]] Imm<3> op2) {
+    return true;
+}
+
+bool TranslatorVisitor::NOP() {
+    return true;
+}
+
+bool TranslatorVisitor::YIELD() {
+    return true;
+}
+
+bool TranslatorVisitor::WFE() {
+    return true;
+}
+
+bool TranslatorVisitor::WFI() {
+    return true;
+}
+
+bool TranslatorVisitor::SEV() {
+    return true;
+}
+
+bool TranslatorVisitor::SEVL() {
+    return true;
+}
+
+} // namespace A64
+} // namespace Dynarmic
