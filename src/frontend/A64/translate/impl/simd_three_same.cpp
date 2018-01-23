b8e26bfdc31354e29d4771c9b24e1cabbfb0b40f
MerryMage <MerryMage@users.noreply.github.com>
2018-01-23 17:44:34 +0000

    A64: Implement ADDP (vector)

--- src/frontend/A64/translate/impl/simd_three_same.cpp
+++ src/frontend/A64/translate/impl/simd_three_same.cpp
@@ -35,6 +35,32 @@ bool TranslatorVisitor::ADD_vector(bool Q, Imm<2> size, Vec Vm, Vec Vn, Vec Vd) 
     return true;
 }
 
+bool TranslatorVisitor::ADDP_vec(bool Q, Imm<2> size, Vec Vm, Vec Vn, Vec Vd) {
+    if (size == 0b11 && !Q) return ReservedValue();
+    const size_t esize = 8 << size.ZeroExtend<size_t>();
+    const size_t datasize = Q ? 128 : 64;
+
+    const IR::U128 operand1 = V(datasize, Vn);
+    const IR::U128 operand2 = V(datasize, Vm);
+
+    const IR::U128 result = [&]{
+        switch (esize) {
+        case 8:
+            return Q ? ir.VectorPairedAdd8(operand1, operand2) : ir.VectorLowerPairedAdd8(operand1, operand2);
+        case 16:
+            return Q ? ir.VectorPairedAdd16(operand1, operand2) : ir.VectorLowerPairedAdd16(operand1, operand2);
+        case 32:
+            return Q ? ir.VectorPairedAdd32(operand1, operand2) : ir.VectorLowerPairedAdd32(operand1, operand2);
+        default:
+            return ir.VectorPairedAdd64(operand1, operand2);
+        }
+    }();
+
+    V(datasize, Vd, result);
+
+    return true;
+}
+
 bool TranslatorVisitor::AND_asimd(bool Q, Vec Vm, Vec Vn, Vec Vd) {
     const size_t datasize = Q ? 128 : 64;
 
