5a65313236271889ff0c239eab3e90609f16dfb2
Lioncash <mathew1800@gmail.com>
2018-02-04 19:21:02 -0500

    A64: Implement CCMP (immediate)

--- src/frontend/A64/translate/impl/data_processing_conditional_compare.cpp
+++ src/frontend/A64/translate/impl/data_processing_conditional_compare.cpp
@@ -47,4 +47,17 @@ bool TranslatorVisitor::CCMN_imm(bool sf, Imm<5> imm5, Cond cond, Reg Rn, Imm<4>
     return true;
 }
 
+bool TranslatorVisitor::CCMP_imm(bool sf, Imm<5> imm5, Cond cond, Reg Rn, Imm<4> nzcv) {
+    const size_t datasize = sf ? 64 : 32;
+    const u32 flags = nzcv.ZeroExtend<u32>() << 28;
+
+    const IR::U32U64 operand1 = X(datasize, Rn);
+    const IR::U32U64 operand2 = I(datasize, imm5.ZeroExtend<u32>());
+
+    const IR::NZCV then_flags = ir.NZCVFrom(ir.AddWithCarry(operand1, ir.Not(operand2), ir.Imm1(1)));
+    const IR::NZCV else_flags = ir.NZCVFromPackedFlags(ir.Imm32(flags));
+    ir.SetNZCV(ir.ConditionalSelect(cond, then_flags, else_flags));
+    return true;
+}
+
 } // namespace Dynarmic::A64
