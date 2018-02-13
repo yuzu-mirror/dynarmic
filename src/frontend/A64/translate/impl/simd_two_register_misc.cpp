8cebb87d0d575189b7fc777a492a2b921a4a084d
MerryMage <MerryMage@users.noreply.github.com>
2018-02-13 19:01:21 +0000

    A64: Implement CMGT (zero), CMEQ (zero), CMLT (zero)

--- src/frontend/A64/translate/impl/simd_two_register_misc.cpp
+++ src/frontend/A64/translate/impl/simd_two_register_misc.cpp
@@ -21,6 +21,51 @@ bool TranslatorVisitor::CNT(bool Q, Imm<2> size, Vec Vn, Vec Vd) {
     return true;
 }
 
+bool TranslatorVisitor::CMGT_zero_2(bool Q, Imm<2> size, Vec Vn, Vec Vd) {
+    if (size == 0b11 && !Q) {
+        return ReservedValue();
+    }
+    const size_t esize = 8 << size.ZeroExtend<size_t>();
+    const size_t datasize = Q ? 128 : 64;
+
+    const IR::U128 operand = V(datasize, Vn);
+    const IR::U128 zero = ir.ZeroExtendToQuad(ir.Imm64(0)); // TODO: Optimize
+    const IR::U128 result = ir.VectorGreaterSigned(esize, operand, zero);
+    V(datasize, Vd, result);
+    return true;
+}
+
+bool TranslatorVisitor::CMEQ_zero_2(bool Q, Imm<2> size, Vec Vn, Vec Vd) {
+    if (size == 0b11 && !Q) {
+        return ReservedValue();
+    }
+    const size_t esize = 8 << size.ZeroExtend<size_t>();
+    const size_t datasize = Q ? 128 : 64;
+
+    const IR::U128 operand = V(datasize, Vn);
+    const IR::U128 zero = ir.ZeroExtendToQuad(ir.Imm64(0)); // TODO: Optimize
+    IR::U128 result = ir.VectorEqual(esize, operand, zero);
+    if (datasize == 64) {
+        result = ir.VectorZeroUpper(result);
+    }
+    V(datasize, Vd, result);
+    return true;
+}
+
+bool TranslatorVisitor::CMLT_2(bool Q, Imm<2> size, Vec Vn, Vec Vd) {
+    if (size == 0b11 && !Q) {
+        return ReservedValue();
+    }
+    const size_t esize = 8 << size.ZeroExtend<size_t>();
+    const size_t datasize = Q ? 128 : 64;
+
+    const IR::U128 operand = V(datasize, Vn);
+    const IR::U128 zero = ir.ZeroExtendToQuad(ir.Imm64(0)); // TODO: Optimize
+    const IR::U128 result = ir.VectorLessSigned(esize, operand, zero);
+    V(datasize, Vd, result);
+    return true;
+}
+
 bool TranslatorVisitor::XTN(bool Q, Imm<2> size, Vec Vn, Vec Vd) {
     if (size == 0b11) {
         return ReservedValue();
