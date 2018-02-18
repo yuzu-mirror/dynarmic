44a5b57f2a575aa4bb7cf2665a420efe06097753
MerryMage <MerryMage@users.noreply.github.com>
2018-02-18 13:47:41 +0000

    fuzz_with_unicorn: QEMU's implementation of FCVT is incorrect

--- tests/A64/fuzz_with_unicorn.cpp
+++ tests/A64/fuzz_with_unicorn.cpp
@@ -109,7 +109,7 @@ static u32 GenFloatInst(u64 pc, bool is_last_inst) {
         // List of instructions not to test
         const std::vector<std::string> do_not_test {
             // QEMU's implementation of FCVT is incorrect
-            "FCVT",
+            "FCVT_float",
         };
 
         std::vector<InstructionGenerator> result;
