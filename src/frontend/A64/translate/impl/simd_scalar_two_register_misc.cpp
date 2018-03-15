6ad1bce5e0826b8d9b1e4500b4f605991e2f142d
Lioncash <mathew1800@gmail.com>
2018-03-15 16:59:48 -0400

    A64: Implement REV16 (vector)

--- src/frontend/A64/translate/impl/simd_scalar_two_register_misc.cpp
+++ src/frontend/A64/translate/impl/simd_scalar_two_register_misc.cpp
@@ -8,6 +8,22 @@
 
 namespace Dynarmic::A64 {
 
+bool TranslatorVisitor::REV16_asimd(bool Q, Imm<2> size, Vec Vn, Vec Vd) {
+    if (size != 0) {
+        return UnallocatedEncoding();
+    }
+
+    const size_t datasize = Q ? 128 : 64;
+    constexpr size_t esize = 16;
+
+    const IR::U128 data = V(datasize, Vn);
+    const IR::U128 result = ir.VectorOr(ir.VectorLogicalShiftRight(esize, data, 8),
+                                        ir.VectorLogicalShiftLeft(esize, data, 8));
+
+    V(datasize, Vd, result);
+    return true;
+}
+
 bool TranslatorVisitor::UCVTF_int_2(bool sz, Vec Vn, Vec Vd) {
     const auto esize = sz ? 64 : 32;
 
