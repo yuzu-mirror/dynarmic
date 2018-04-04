0d83032a6fec19bcc5990090ebc7cf5a3bff79f2
MerryMage <MerryMage@users.noreply.github.com>
2018-04-04 19:00:28 +0100

    A64: Implement FMSUB

--- src/frontend/A64/translate/impl/floating_point_data_processing_three_register.cpp
+++ src/frontend/A64/translate/impl/floating_point_data_processing_three_register.cpp
@@ -37,4 +37,18 @@ bool TranslatorVisitor::FMADD_float(Imm<2> type, Vec Vm, Vec Va, Vec Vn, Vec Vd)
     return true;
 }
 
+bool TranslatorVisitor::FMSUB_float(Imm<2> type, Vec Vm, Vec Va, Vec Vn, Vec Vd) {
+    const auto datasize = GetDataSize(type);
+    if (!datasize || *datasize == 16) {
+        return UnallocatedEncoding();
+    }
+
+    const IR::U32U64 operanda = V_scalar(*datasize, Va);
+    const IR::U32U64 operand1 = V_scalar(*datasize, Vn);
+    const IR::U32U64 operand2 = V_scalar(*datasize, Vm);
+    const IR::U32U64 result = ir.FPMulAdd(operanda, ir.FPNeg(operand1), operand2, true);
+    V_scalar(*datasize, Vd, result);
+    return true;
+}
+
 } // namespace Dynarmic::A64
