5c9e7f328d771496b067d46305384cbc534bb8fd
Lioncash <mathew1800@gmail.com>
2018-04-05 09:35:57 -0400

    A64: Implement USUBL/USUBL2

--- src/frontend/A64/translate/impl/simd_three_different.cpp
+++ src/frontend/A64/translate/impl/simd_three_different.cpp
@@ -90,4 +90,20 @@ bool TranslatorVisitor::USUBW(bool Q, Imm<2> size, Vec Vm, Vec Vn, Vec Vd) {
     return true;
 }
 
+bool TranslatorVisitor::USUBL(bool Q, Imm<2> size, Vec Vm, Vec Vn, Vec Vd) {
+    if (size == 0b11) {
+        return ReservedValue();
+    }
+
+    const size_t esize = 8 << size.ZeroExtend();
+    const size_t part = Q ? 1 : 0;
+
+    const IR::U128 operand1 = ir.VectorZeroExtend(esize, Vpart(64, Vn, part));
+    const IR::U128 operand2 = ir.VectorZeroExtend(esize, Vpart(64, Vm, part));
+    const IR::U128 result = ir.VectorSub(esize * 2, operand1, operand2);
+
+    V(128, Vd, result);
+    return true;
+}
+
 } // namespace Dynarmic::A64
