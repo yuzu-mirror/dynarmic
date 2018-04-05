88d70e3b8aa05f9e32f9432590d3aac84826b35c
Lioncash <mathew1800@gmail.com>
2018-04-05 09:28:50 -0400

    A64: Implement UADDL/UADDL2

--- src/frontend/A64/translate/impl/simd_three_different.cpp
+++ src/frontend/A64/translate/impl/simd_three_different.cpp
@@ -40,6 +40,22 @@ bool TranslatorVisitor::SSUBW(bool Q, Imm<2> size, Vec Vm, Vec Vn, Vec Vd) {
     return true;
 }
 
+bool TranslatorVisitor::UADDL(bool Q, Imm<2> size, Vec Vm, Vec Vn, Vec Vd) {
+    if (size == 0b11) {
+        return ReservedValue();
+    }
+
+    const size_t esize = 8 << size.ZeroExtend();
+    const size_t part = Q ? 1 : 0;
+
+    const IR::U128 operand1 = ir.VectorZeroExtend(esize, Vpart(64, Vn, part));
+    const IR::U128 operand2 = ir.VectorZeroExtend(esize, Vpart(64, Vm, part));
+    const IR::U128 result = ir.VectorAdd(esize * 2, operand1, operand2);
+
+    V(128, Vd, result);
+    return true;
+}
+
 bool TranslatorVisitor::UADDW(bool Q, Imm<2> size, Vec Vm, Vec Vn, Vec Vd) {
     if (size == 0b11) {
         return ReservedValue();
