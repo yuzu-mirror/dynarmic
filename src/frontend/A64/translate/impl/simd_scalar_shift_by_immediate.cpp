d56fa8f735ab9e073e6b7242ad00be2aad242a72
Lioncash <mathew1800@gmail.com>
2018-04-12 14:19:42 -0400

    A64: Implement USHR (scalar)

--- src/frontend/A64/translate/impl/simd_scalar_shift_by_immediate.cpp
+++ src/frontend/A64/translate/impl/simd_scalar_shift_by_immediate.cpp
@@ -8,6 +8,21 @@
 
 namespace Dynarmic::A64 {
 
+static void ShiftRight(TranslatorVisitor& v, Imm<4> immh, Imm<3> immb, Vec Vn, Vec Vd) {
+    const size_t esize = 64;
+    const u8 shift_amount = static_cast<u8>((esize * 2) - concatenate(immh, immb).ZeroExtend());
+
+    const IR::U64 operand = v.V_scalar(esize, Vn);
+    const IR::U64 result = [&] {
+        if (shift_amount == esize) {
+            return v.ir.Imm64(0);
+        }
+        return v.ir.LogicalShiftRight(operand, v.ir.Imm8(shift_amount));
+    }();
+
+    v.V_scalar(esize, Vd, result);
+}
+
 bool TranslatorVisitor::SHL_1(Imm<4> immh, Imm<3> immb, Vec Vn, Vec Vd) {
     if (!immh.Bit<3>()) {
         return ReservedValue();
@@ -20,6 +35,15 @@ bool TranslatorVisitor::SHL_1(Imm<4> immh, Imm<3> immb, Vec Vn, Vec Vd) {
     const IR::U64 result = ir.LogicalShiftLeft(operand, ir.Imm8(shift_amount));
 
     V_scalar(esize, Vd, result);
+    return true;
+}
+
+bool TranslatorVisitor::USHR_1(Imm<4> immh, Imm<3> immb, Vec Vn, Vec Vd) {
+    if (!immh.Bit<3>()) {
+        return ReservedValue();
+    }
+
+    ShiftRight(*this, immh, immb, Vn, Vd);
     return true;
 }
 
