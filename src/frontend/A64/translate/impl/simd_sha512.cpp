e268b110f0795d2a852f4fe7e10a38591613fad5
Lioncash <mathew1800@gmail.com>
2018-04-12 08:03:46 -0400

    simd_sha512: Simplify RAX1
    Now that the vector rotation helpers are in, replace the explicit
    shifting with the relevant helper function that does the same thing.
    
    Simply tidies up code; no behavioral changes are made.

--- src/frontend/A64/translate/impl/simd_sha512.cpp
+++ src/frontend/A64/translate/impl/simd_sha512.cpp
@@ -12,8 +12,7 @@ bool TranslatorVisitor::RAX1(Vec Vm, Vec Vn, Vec Vd) {
     const IR::U128 m = ir.GetQ(Vm);
     const IR::U128 n = ir.GetQ(Vn);
 
-    const IR::U128 rotated_m = ir.VectorOr(ir.VectorLogicalShiftLeft(64, m, 1),
-                                           ir.VectorLogicalShiftRight(64, m, 63));
+    const IR::U128 rotated_m = ir.VectorRotateLeft(64, m, 1);
     const IR::U128 result = ir.VectorEor(n, rotated_m);
 
     ir.SetQ(Vd, result);
