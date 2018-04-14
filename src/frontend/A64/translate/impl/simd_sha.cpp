205ca6b4cb95d62fa2b66637e9ce33a587566528
Lioncash <mathew1800@gmail.com>
2018-04-13 20:39:25 -0400

    A64: Implement SHA1SU1

--- src/frontend/A64/translate/impl/simd_sha.cpp
+++ src/frontend/A64/translate/impl/simd_sha.cpp
@@ -28,6 +28,23 @@ bool TranslatorVisitor::SHA1SU0(Vec Vm, Vec Vn, Vec Vd) {
     return true;
 }
 
+bool TranslatorVisitor::SHA1SU1(Vec Vn, Vec Vd) {
+    const IR::U128 d = ir.GetQ(Vd);
+    const IR::U128 n = ir.GetQ(Vn);
+
+    // Shuffle down the whole vector and zero out the top 32 bits
+    const IR::U128 shuffled_n = ir.VectorSetElement(32, ir.VectorShuffleWords(n, 0b00111001), 3, ir.Imm32(0));
+    const IR::U128 t = ir.VectorEor(d, shuffled_n);
+    const IR::U128 rotated_t = ir.VectorRotateLeft(32, t, 1);
+
+    const IR::U32 low_rotated_t = ir.RotateRight(ir.VectorGetElement(32, rotated_t, 0), ir.Imm8(31));
+    const IR::U32 high_t = ir.VectorGetElement(32, rotated_t, 3);
+    const IR::U128 result = ir.VectorSetElement(32, rotated_t, 3, ir.Eor(low_rotated_t, high_t));
+
+    ir.SetQ(Vd, result);
+    return true;
+}
+
 bool TranslatorVisitor::SHA1H(Vec Vn, Vec Vd) {
     const IR::U128 data = ir.GetS(Vn);
 
