16a001b9ff5153c0d1834eb3624404da5e7f1d41
Lioncash <mathew1800@gmail.com>
2018-04-13 20:22:02 -0400

    A64: Implement SHA1SU0

--- src/frontend/A64/translate/impl/simd_sha.cpp
+++ src/frontend/A64/translate/impl/simd_sha.cpp
@@ -8,6 +8,26 @@
 
 namespace Dynarmic::A64 {
 
+bool TranslatorVisitor::SHA1SU0(Vec Vm, Vec Vn, Vec Vd) {
+    const IR::U128 d = ir.GetQ(Vd);
+    const IR::U128 m = ir.GetQ(Vm);
+    const IR::U128 n = ir.GetQ(Vn);
+
+    IR::U128 result = [&] {
+        const IR::U64 d_high = ir.VectorGetElement(64, d, 1);
+        const IR::U64 n_low = ir.VectorGetElement(64, n, 0);
+        const IR::U128 zero = ir.ZeroVector();
+
+        const IR::U128 tmp1 = ir.VectorSetElement(64, zero, 0, d_high);
+        return ir.VectorSetElement(64, tmp1, 1, n_low);
+    }();
+
+    result = ir.VectorEor(ir.VectorEor(result, d), m);
+
+    ir.SetQ(Vd, result);
+    return true;
+}
+
 bool TranslatorVisitor::SHA1H(Vec Vn, Vec Vd) {
     const IR::U128 data = ir.GetS(Vn);
 
