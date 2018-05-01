51e4f1d9dbe158ad2f50b9dfee69a8afdb39e2fe
Lioncash <mathew1800@gmail.com>
2018-05-01 08:22:53 -0400

    emit_x64_vector: Vectorize fallback path of EmitVectorMaxS32()

--- src/backend_x64/emit_x64_vector.cpp
+++ src/backend_x64/emit_x64_vector.cpp
@@ -1026,9 +1026,19 @@ void EmitX64::EmitVectorMaxS32(EmitContext& ctx, IR::Inst* inst) {
         return;
     }
 
-    EmitTwoArgumentFallback(code, ctx, inst, [](std::array<s32, 4>& result, const std::array<s32, 4>& a, const std::array<s32, 4>& b){
-        std::transform(a.begin(), a.end(), b.begin(), result.begin(), [](auto x, auto y) { return std::max(x, y); });
-    });
+    auto args = ctx.reg_alloc.GetArgumentInfo(inst);
+    const Xbyak::Xmm a = ctx.reg_alloc.UseScratchXmm(args[0]);
+    const Xbyak::Xmm b = ctx.reg_alloc.UseScratchXmm(args[1]);
+
+    const Xbyak::Xmm tmp_b = ctx.reg_alloc.ScratchXmm();
+    code.movdqa(tmp_b, b);
+
+    code.pcmpgtd(tmp_b, a);
+    code.pand(b, tmp_b);
+    code.pandn(tmp_b, a);
+    code.por(tmp_b, b);
+
+    ctx.reg_alloc.DefineValue(inst, tmp_b);
 }
 
 void EmitX64::EmitVectorMaxS64(EmitContext& ctx, IR::Inst* inst) {
