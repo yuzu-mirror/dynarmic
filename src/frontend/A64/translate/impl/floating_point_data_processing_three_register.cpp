69e00d225cfa2ea6057e8be63c2b7de91775d066
MerryMage <MerryMage@users.noreply.github.com>
2018-06-06 19:57:58 +0100

    A64: Implement FMADD

---
+++ src/frontend/A64/translate/impl/floating_point_data_processing_three_register.cpp
@@ -0,0 +1,40 @@
+/* This file is part of the dynarmic project.
+ * Copyright (c) 2018 MerryMage
+ * This software may be used and distributed according to the terms of the GNU
+ * General Public License version 2 or any later version.
+ */
+
+#include <boost/optional.hpp>
+
+#include "frontend/A64/translate/impl/impl.h"
+
+namespace Dynarmic::A64 {
+
+static boost::optional<size_t> GetDataSize(Imm<2> type) {
+    switch (type.ZeroExtend()) {
+    case 0b00:
+        return 32;
+    case 0b01:
+        return 64;
+    case 0b11:
+        // FP16Ext, unimplemented.
+        return boost::none;
+    }
+    return boost::none;
+}
+
+bool TranslatorVisitor::FMADD_float(Imm<2> type, Vec Vm, Vec Va, Vec Vn, Vec Vd) {
+    const auto datasize = GetDataSize(type);
+    if (!datasize || *datasize == 16) {
+        return UnallocatedEncoding();
+    }
+
+    const IR::U32U64 operanda = V_scalar(*datasize, Va);
+    const IR::U32U64 operand1 = V_scalar(*datasize, Vn);
+    const IR::U32U64 operand2 = V_scalar(*datasize, Vm);
+    const IR::U32U64 result = ir.FPMulAdd(operanda, operand1, operand2, true);
+    V_scalar(*datasize, Vd, result);
+    return true;
+}
+
+} // namespace Dynarmic::A64
