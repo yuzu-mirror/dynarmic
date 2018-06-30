af661ef5a647d0e0b54e9d08756010bc218c9886
MerryMage <MerryMage@users.noreply.github.com>
2018-06-30 12:18:40 +0100

    A64: Implement FCVTPS (scalar)

--- src/frontend/A64/translate/impl/floating_point_conversion_integer.cpp
+++ src/frontend/A64/translate/impl/floating_point_conversion_integer.cpp
@@ -214,4 +214,8 @@ bool TranslatorVisitor::FCVTAU_float(bool sf, Imm<2> type, Vec Vn, Reg Rd) {
     return FloaingPointConvertUnsignedInteger(*this, sf, type, Vn, Rd, FP::RoundingMode::ToNearest_TieAwayFromZero);
 }
 
+bool TranslatorVisitor::FCVTPS_float(bool sf, Imm<2> type, Vec Vn, Reg Rd) {
+    return FloaingPointConvertSignedInteger(*this, sf, type, Vn, Rd, FP::RoundingMode::TowardsPlusInfinity);
+}
+
 } // namespace Dynarmic::A64
