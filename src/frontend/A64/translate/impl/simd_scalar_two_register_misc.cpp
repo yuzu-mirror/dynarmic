f7f83b76b729382f12e78464ff67119a55855a3c
Lioncash <mathew1800@gmail.com>
2018-07-15 19:50:32 -0400

    simd_scalar_two_register_misc: Implement scalar double/single-precision variants of FCM{EQ, GE, GT, LE, LT} (zero)

--- src/frontend/A64/translate/impl/simd_scalar_two_register_misc.cpp
+++ src/frontend/A64/translate/impl/simd_scalar_two_register_misc.cpp
@@ -7,6 +7,43 @@
 #include "frontend/A64/translate/impl/impl.h"
 
 namespace Dynarmic::A64 {
+namespace {
+enum class ComparisonType {
+    EQ,
+    GE,
+    GT,
+    LE,
+    LT
+};
+
+bool ScalarFPCompareAgainstZero(TranslatorVisitor& v, bool sz, Vec Vn, Vec Vd, ComparisonType type) {
+    const size_t esize = sz ? 64 : 32;
+    const size_t datasize = esize;
+
+    const IR::U128 operand = v.V(datasize, Vn);
+    const IR::U128 zero = v.ir.ZeroVector();
+    const IR::U128 result = [&] {
+        switch (type) {
+        case ComparisonType::EQ:
+            return v.ir.FPVectorEqual(esize, operand, zero);
+        case ComparisonType::GE:
+            return v.ir.FPVectorGreaterEqual(esize, operand, zero);
+        case ComparisonType::GT:
+            return v.ir.FPVectorGreater(esize, operand, zero);
+        case ComparisonType::LE:
+            return v.ir.FPVectorGreaterEqual(esize, zero, operand);
+        case ComparisonType::LT:
+            return v.ir.FPVectorGreater(esize, zero, operand);
+        }
+
+        UNREACHABLE();
+        return IR::U128{};
+    }();
+
+    v.V_scalar(datasize, Vd, v.ir.VectorGetElement(esize, result, 0));
+    return true;
+}
+} // Anonymous namespace
 
 bool TranslatorVisitor::ABS_1(Imm<2> size, Vec Vn, Vec Vd) {
     if (size != 0b11) {
@@ -19,6 +56,26 @@ bool TranslatorVisitor::ABS_1(Imm<2> size, Vec Vn, Vec Vd) {
 
     V_scalar(64, Vd, result);
     return true;
+}
+
+bool TranslatorVisitor::FCMEQ_zero_2(bool sz, Vec Vn, Vec Vd) {
+    return ScalarFPCompareAgainstZero(*this, sz, Vn, Vd, ComparisonType::EQ);
+}
+
+bool TranslatorVisitor::FCMGE_zero_2(bool sz, Vec Vn, Vec Vd) {
+    return ScalarFPCompareAgainstZero(*this, sz, Vn, Vd, ComparisonType::GE);
+}
+
+bool TranslatorVisitor::FCMGT_zero_2(bool sz, Vec Vn, Vec Vd) {
+    return ScalarFPCompareAgainstZero(*this, sz, Vn, Vd, ComparisonType::GT);
+}
+
+bool TranslatorVisitor::FCMLE_2(bool sz, Vec Vn, Vec Vd) {
+    return ScalarFPCompareAgainstZero(*this, sz, Vn, Vd, ComparisonType::LE);
+}
+
+bool TranslatorVisitor::FCMLT_2(bool sz, Vec Vn, Vec Vd) {
+    return ScalarFPCompareAgainstZero(*this, sz, Vn, Vd, ComparisonType::LT);
 }
 
 bool TranslatorVisitor::NEG_1(Imm<2> size, Vec Vn, Vec Vd) {
