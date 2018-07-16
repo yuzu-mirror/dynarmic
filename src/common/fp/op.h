797e18cd9738eeb77eaf8b04479c79f9a6cea0be
MerryMage <MerryMage@users.noreply.github.com>
2018-07-16 12:51:36 +0100

    fp: Move FPToFixed to its own file

--- src/common/fp/op.h
+++ src/common/fp/op.h
@@ -6,16 +6,4 @@
 
 #pragma once
 
-#include "common/common_types.h"
-#include "common/fp/fpsr.h"
-#include "common/fp/rounding_mode.h"
-#include "frontend/A64/FPCR.h"
-
-namespace Dynarmic::FP {
-
-using FPCR = A64::FPCR;
-
-template<typename FPT>
-u64 FPToFixed(size_t ibits, FPT op, size_t fbits, bool unsigned_, FPCR fpcr, RoundingMode rounding, FPSR& fpsr);
-
-} // namespace Dynarmic::FP 
+#include "common/fp/op/FPToFixed.h"
