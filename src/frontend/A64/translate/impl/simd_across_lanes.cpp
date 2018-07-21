a1bc8ddb535ff9d1ae520735d3ec3e968f2bbd02
Lioncash <mathew1800@gmail.com>
2018-07-21 19:53:21 -0400

    A64: Implement UADDLV

--- src/frontend/A64/translate/impl/simd_across_lanes.cpp
+++ src/frontend/A64/translate/impl/simd_across_lanes.cpp
@@ -39,4 +39,35 @@ bool TranslatorVisitor::ADDV(bool Q, Imm<2> size, Vec Vn, Vec Vd) {
     return true;
 }
 
+bool TranslatorVisitor::UADDLV(bool Q, Imm<2> size, Vec Vn, Vec Vd) {
+    if ((size == 0b10 && !Q) || size == 0b11) {
+        return ReservedValue();
+    }
+
+    const size_t esize = 8 << size.ZeroExtend();
+    const size_t datasize = Q ? 128 : 64;
+    const size_t elements = datasize / esize;
+
+    const IR::U128 operand = V(datasize, Vn);
+
+    const auto get_element = [&](IR::U128 vec, size_t element) {
+        return ir.ZeroExtendToLong(ir.VectorGetElement(esize, vec, element));
+    };
+
+    IR::U64 sum = get_element(operand, 0);
+    for (size_t i = 1; i < elements; i++) {
+        sum = ir.Add(sum, get_element(operand, i));
+    }
+
+    if (size == 0b00) {
+        V(datasize, Vd, ir.ZeroExtendToQuad(ir.LeastSignificantHalf(sum)));
+    } else if (size == 0b01) {
+        V(datasize, Vd, ir.ZeroExtendToQuad(ir.LeastSignificantWord(sum)));
+    } else {
+        V(datasize, Vd, ir.ZeroExtendToQuad(sum));
+    }
+
+    return true;
+}
+
 } // namespace Dynarmic::A64
