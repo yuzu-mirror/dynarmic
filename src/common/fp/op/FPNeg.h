b0ff35fcd1f41575d1f89eb962724335ae4bd3ad
MerryMage <MerryMage@users.noreply.github.com>
2018-07-23 22:01:25 +0100

    fp: Implement FPNeg

---
+++ src/common/fp/op/FPNeg.h
@@ -0,0 +1,18 @@
+/* This file is part of the dynarmic project.
+ * Copyright (c) 2018 MerryMage
+ * This software may be used and distributed according to the terms of the GNU
+ * General Public License version 2 or any later version.
+ */
+
+#pragma once
+
+#include "common/fp/info.h"
+
+namespace Dynarmic::FP {
+
+template<typename FPT>
+inline FPT FPNeg(FPT op) {
+    return op ^ FPInfo<FPT>::sign_mask;
+}
+
+} // namespace Dynarmic::FP 
