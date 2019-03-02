e44730ba6d8f2ee575de27146833906df7b99a2d
Lioncash <mathew1800@gmail.com>
2019-03-02 18:56:37 -0500

    A64: Implement FRECPX (single, double precision)

--- src/frontend/A64/translate/impl/simd_scalar_two_register_misc.cpp
+++ src/frontend/A64/translate/impl/simd_scalar_two_register_misc.cpp
@@ -170,6 +170,16 @@ bool TranslatorVisitor::FRECPE_2(bool sz, Vec Vn, Vec Vd) {
     return true;
 }
 
+bool TranslatorVisitor::FRECPX_2(bool sz, Vec Vn, Vec Vd) {
+    const size_t esize = sz ? 64 : 32;
+
+    const IR::U32U64 operand = V_scalar(esize, Vn);
+    const IR::U32U64 result = ir.FPRecipExponent(operand);
+
+    V_scalar(esize, Vd, result);
+    return true;
+}
+
 bool TranslatorVisitor::FRSQRTE_2(bool sz, Vec Vn, Vec Vd) {
     const size_t esize = sz ? 64 : 32;
 
