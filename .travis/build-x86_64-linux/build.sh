d4a531c21f157e7b4bd5d3777f9d673fad690914
Lioncash <mathew1800@gmail.com>
2019-04-20 05:48:37 -0400

    travis: Re-enable A32 tests

--- .travis/build-x86_64-linux/build.sh
+++ .travis/build-x86_64-linux/build.sh
@@ -11,4 +11,4 @@ mkdir build && cd build
 cmake .. -DBoost_INCLUDE_DIRS=${PWD}/../externals/ext-boost -DCMAKE_BUILD_TYPE=Release -G Ninja
 ninja
 
-./tests/dynarmic_tests --durations yes [a64]
+./tests/dynarmic_tests --durations yes
--- .travis/sse3-only-on-x86_64-linux/deps.sh
+++ .travis/sse3-only-on-x86_64-linux/deps.sh
@@ -8,7 +8,7 @@ cd externals
 git clone https://github.com/MerryMage/ext-boost
 git clone https://github.com/yuzu-emu/unicorn
 cd unicorn
-UNICORN_ARCHS=aarch64 ./make.sh
+UNICORN_ARCHS=aarch64,arm ./make.sh
 cd ../..
 
 mkdir -p $HOME/.local
