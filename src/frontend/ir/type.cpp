e4c65721fed291a2c14e9edb4566549bcaa1fff2
Lioncash <mathew1800@gmail.com>
2019-05-02 09:08:21 -0400

    frontend/ir/type: Generify std::array declaration
    With deduction guides, we can eliminate the need to explicitly size the
    array. Also newlines the elements based off their relation, making it
    slightly nicer to read.

--- src/frontend/ir/type.cpp
+++ src/frontend/ir/type.cpp
@@ -16,7 +16,16 @@
 namespace Dynarmic::IR {
 
 std::string GetNameOf(Type type) {
-    static const std::array<const char*, 15> names{"A32Reg", "A32ExtReg", "A64Reg", "A64Vec", "Opaque", "U1", "U8", "U16", "U32", "U64", "U128", "CoprocInfo", "NZCVFlags", "Cond", "Table"};
+    static constexpr std::array names{
+        "A32Reg", "A32ExtReg",
+        "A64Reg", "A64Vec",
+        "Opaque",
+        "U1", "U8", "U16", "U32", "U64", "U128",
+        "CoprocInfo",
+        "NZCVFlags",
+        "Cond",
+        "Table"
+    };
 
     const size_t bits = static_cast<size_t>(type);
     if (bits == 0) {
