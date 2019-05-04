aba4d8a59f13c63b5d97c8e0855134415a68ef5d
Lioncash <mathew1800@gmail.com>
2019-05-03 22:44:37 -0400

    dynarmic_tests: Use variable template equivalents of type traits where applicable
    Same thing, same readability, less writing.

--- tests/rand_int.h
+++ tests/rand_int.h
@@ -9,8 +9,8 @@
 
 template <typename T>
 T RandInt(T min, T max) {
-    static_assert(std::is_integral<T>::value, "T must be an integral type.");
-    static_assert(!std::is_same<T, signed char>::value && !std::is_same<T, unsigned char>::value,
+    static_assert(std::is_integral_v<T>, "T must be an integral type.");
+    static_assert(!std::is_same_v<T, signed char> && !std::is_same_v<T, unsigned char>,
                   "Using char with uniform_int_distribution is undefined behavior.");
 
     static std::random_device rd;
