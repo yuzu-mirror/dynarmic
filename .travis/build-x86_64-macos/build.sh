7be003818693ab41039aa0eecee869cf7186173a
MerryMage <MerryMage@users.noreply.github.com>
2019-08-14 20:59:13 +0100

    Reduce requirements to macOS 10.12

--- .travis/build-x86_64-macos/build.sh
+++ .travis/build-x86_64-macos/build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 set -o pipefail
 
-export MACOSX_DEPLOYMENT_TARGET=10.14
+export MACOSX_DEPLOYMENT_TARGET=10.12
 
 mkdir build && cd build
 cmake .. -GXcode -DBoost_INCLUDE_DIRS=${PWD}/../externals/ext-boost -DDYNARMIC_TESTS=0
