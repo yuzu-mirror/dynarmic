67b284f6fa4860d1c3b703275c2f56860443d80f
Lioncash <mathew1800@gmail.com>
2020-05-16 13:00:32 -0400

    A32: Implement ASIMD VORN (register)

--- src/frontend/A32/translate/impl/translate_arm.h
+++ src/frontend/A32/translate/impl/translate_arm.h
@@ -433,6 +433,7 @@ struct ArmTranslatorVisitor final {
     bool asimd_VAND_reg(bool D, size_t Vn, size_t Vd, bool N, bool Q, bool M, size_t Vm);
     bool asimd_VBIC_reg(bool D, size_t Vn, size_t Vd, bool N, bool Q, bool M, size_t Vm);
     bool asimd_VORR_reg(bool D, size_t Vn, size_t Vd, bool N, bool Q, bool M, size_t Vm);
+    bool asimd_VORN_reg(bool D, size_t Vn, size_t Vd, bool N, bool Q, bool M, size_t Vm);
 
     // Advanced SIMD load/store structures
     bool v8_VLD_multiple(bool D, Reg n, size_t Vd, Imm<4> type, size_t sz, size_t align, Reg m);
