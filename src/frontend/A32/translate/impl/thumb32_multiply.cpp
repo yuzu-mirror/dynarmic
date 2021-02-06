ed453aa52ddd29e52c09f3afcc61e11e7370d52f
Lioncash <mathew1800@gmail.com>
2021-02-06 18:58:54 -0500

    thumb32: Implement USAD8

--- src/frontend/A32/translate/impl/thumb32_multiply.cpp
+++ src/frontend/A32/translate/impl/thumb32_multiply.cpp
@@ -48,4 +48,17 @@ bool ThumbTranslatorVisitor::thumb32_MUL(Reg n, Reg d, Reg m) {
     return true;
 }
 
+bool ThumbTranslatorVisitor::thumb32_USAD8(Reg n, Reg d, Reg m) {
+    if (d == Reg::PC || n == Reg::PC || m == Reg::PC) {
+        return UnpredictableInstruction();
+    }
+
+    const auto reg_m = ir.GetRegister(m);
+    const auto reg_n = ir.GetRegister(n);
+    const auto result = ir.PackedAbsDiffSumS8(reg_n, reg_m);
+
+    ir.SetRegister(d, result);
+    return true;
+}
+
 } // namespace Dynarmic::A32
