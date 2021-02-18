6f3c5dc1d93b28d9dbda865bd1ad7d70781141f1
MerryMage <MerryMage@users.noreply.github.com>
2021-02-18 01:11:06 +0000

    thumb32: Implement AND (immediate)

--- src/frontend/A32/translate/impl/thumb32_data_processing_modified_immediate.cpp
+++ src/frontend/A32/translate/impl/thumb32_data_processing_modified_immediate.cpp
@@ -12,12 +12,30 @@ bool ThumbTranslatorVisitor::thumb32_TST_imm(Imm<1> i, Reg n, Imm<3> imm3, Imm<8
         return UnpredictableInstruction();
     }
 
-    const auto imm_carry = ThumbExpandImm_C(rotate, imm8, ir.GetCFlag());
+    const auto imm_carry = ThumbExpandImm_C(i, imm3, imm8, ir.GetCFlag());
     const auto result = ir.And(ir.GetRegister(n), ir.Imm32(imm_carry.imm32));
 
     ir.SetNFlag(ir.MostSignificantBit(result));
     ir.SetZFlag(ir.IsZero(result));
     ir.SetCFlag(imm_carry.carry);
+    return true;
+}
+
+bool ThumbTranslatorVisitor::thumb32_AND_imm(Imm<1> i, bool S, Reg n, Imm<3> imm3, Reg d, Imm<8> imm8) {
+    ASSERT_MSG(!(d == Reg::PC && S), "Decode error");
+    if ((d == Reg::PC && !S) || n == Reg::PC) {
+        return UnpredictableInstruction();
+    }
+
+    const auto imm_carry = ThumbExpandImm_C(i, imm3, imm8, ir.GetCFlag());
+    const auto result = ir.And(ir.GetRegister(n), ir.Imm32(imm_carry.imm32));
+
+    ir.SetRegister(d, result);
+    if (S) {
+        ir.SetNFlag(ir.MostSignificantBit(result));
+        ir.SetZFlag(ir.IsZero(result));
+        ir.SetCFlag(imm_carry.carry);
+    }
     return true;
 }
 
