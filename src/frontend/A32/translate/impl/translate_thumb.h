b2f0575fee3857e536de6b9289196e862ff3932b
MerryMage <MerryMage@users.noreply.github.com>
2021-02-18 01:19:11 +0000

    thumb32: Implement MOV (immediate)

--- src/frontend/A32/translate/impl/translate_thumb.h
+++ src/frontend/A32/translate/impl/translate_thumb.h
@@ -152,6 +152,7 @@ struct ThumbTranslatorVisitor final {
     bool thumb32_TST_imm(Imm<1> i, Reg n, Imm<3> imm3, Imm<8> imm8);
     bool thumb32_AND_imm(Imm<1> i, bool S, Reg n, Imm<3> imm3, Reg d, Imm<8> imm8);
     bool thumb32_BIC_imm(Imm<1> i, bool S, Reg n, Imm<3> imm3, Reg d, Imm<8> imm8);
+    bool thumb32_MOV_imm(Imm<1> i, bool S, Imm<3> imm3, Reg d, Imm<8> imm8);
 
     // thumb32 miscellaneous control instructions
     bool thumb32_UDF();
