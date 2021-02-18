3dcc882fbf6a14e7a0240cd6a5cdd0a428ee2422
MerryMage <MerryMage@users.noreply.github.com>
2021-02-18 01:12:52 +0000

    thumb32: Implement BIC (immediate)

--- src/frontend/A32/translate/impl/thumb32_data_processing_modified_immediate.cpp
+++ src/frontend/A32/translate/impl/thumb32_data_processing_modified_immediate.cpp
@@ -39,4 +39,21 @@ bool ThumbTranslatorVisitor::thumb32_AND_imm(Imm<1> i, bool S, Reg n, Imm<3> imm
     return true;
 }
 
+bool ThumbTranslatorVisitor::thumb32_BIC_imm(Imm<1> i, bool S, Reg n, Imm<3> imm3, Reg d, Imm<8> imm8) {
+    if (d == Reg::PC || n == Reg::PC) {
+        return UnpredictableInstruction();
+    }
+
+    const auto imm_carry = ThumbExpandImm_C(i, imm3, imm8, ir.GetCFlag());
+    const auto result = ir.And(ir.GetRegister(n), ir.Not(ir.Imm32(imm_carry.imm32)));
+
+    ir.SetRegister(d, result);
+    if (S) {
+        ir.SetNFlag(ir.MostSignificantBit(result));
+        ir.SetZFlag(ir.IsZero(result));
+        ir.SetCFlag(imm_carry.carry);
+    }
+    return true;
+}
+
 } // namespace Dynarmic::A32
