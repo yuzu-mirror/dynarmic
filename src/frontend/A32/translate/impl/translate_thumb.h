a7a9ed69b76d80222b34c92e3f93aca313a49c31
Lioncash <mathew1800@gmail.com>
2021-02-23 04:46:06 -0500

    thumb32: Implement ROR (register)

--- src/frontend/A32/translate/impl/translate_thumb.h
+++ src/frontend/A32/translate/impl/translate_thumb.h
@@ -185,6 +185,7 @@ struct ThumbTranslatorVisitor final {
     bool thumb32_ASR_reg(Reg m, Reg d, Reg s);
     bool thumb32_LSL_reg(Reg m, Reg d, Reg s);
     bool thumb32_LSR_reg(Reg m, Reg d, Reg s);
+    bool thumb32_ROR_reg(Reg m, Reg d, Reg s);
     bool thumb32_SXTB(Reg d, SignExtendRotation rotate, Reg m);
     bool thumb32_SXTB16(Reg d, SignExtendRotation rotate, Reg m);
     bool thumb32_SXTAB(Reg n, Reg d, SignExtendRotation rotate, Reg m);
