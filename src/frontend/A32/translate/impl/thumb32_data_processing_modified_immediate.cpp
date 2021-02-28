78330e634f3e4ab4c7fbfa94331c2ccbd5449889
MerryMage <MerryMage@users.noreply.github.com>
2021-02-28 21:42:06 +0000

    thumb32: Implement SBC (immediate)

--- src/frontend/A32/translate/impl/thumb32_data_processing_modified_immediate.cpp
+++ src/frontend/A32/translate/impl/thumb32_data_processing_modified_immediate.cpp
@@ -210,4 +210,22 @@ bool ThumbTranslatorVisitor::thumb32_ADC_imm(Imm<1> i, bool S, Reg n, Imm<3> imm
     return true;
 }
 
+bool ThumbTranslatorVisitor::thumb32_SBC_imm(Imm<1> i, bool S, Reg n, Imm<3> imm3, Reg d, Imm<8> imm8) {
+    if (d == Reg::PC || n == Reg::PC) {
+        return UnpredictableInstruction();
+    }
+
+    const auto imm32 = ThumbExpandImm(i, imm3, imm8);
+    const auto result = ir.SubWithCarry(ir.GetRegister(n), ir.Imm32(imm32), ir.GetCFlag());
+
+    ir.SetRegister(d, result.result);
+    if (S) {
+        ir.SetNFlag(ir.MostSignificantBit(result.result));
+        ir.SetZFlag(ir.IsZero(result.result));
+        ir.SetCFlag(result.carry);
+        ir.SetVFlag(result.overflow);
+    }
+    return true;
+}
+
 } // namespace Dynarmic::A32
