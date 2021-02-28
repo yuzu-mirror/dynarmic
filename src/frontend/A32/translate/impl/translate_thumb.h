e6b925146bed031c120a10a3bf95e73e30347abd
MerryMage <MerryMage@users.noreply.github.com>
2021-02-28 21:41:08 +0000

    thumb32: Implement ADC (immediate)

--- src/frontend/A32/translate/impl/translate_thumb.h
+++ src/frontend/A32/translate/impl/translate_thumb.h
@@ -160,6 +160,7 @@ struct ThumbTranslatorVisitor final {
     bool thumb32_EOR_imm(Imm<1> i, bool S, Reg n, Imm<3> imm3, Reg d, Imm<8> imm8);
     bool thumb32_CMN_imm(Imm<1> i, Reg n, Imm<3> imm3, Imm<8> imm8);
     bool thumb32_ADD_imm_1(Imm<1> i, bool S, Reg n, Imm<3> imm3, Reg d, Imm<8> imm8);
+    bool thumb32_ADC_imm(Imm<1> i, bool S, Reg n, Imm<3> imm3, Reg d, Imm<8> imm8);
 
     // thumb32 data processing (plain binary immediate) instructions.
     bool thumb32_MOVT(Imm<1> imm1, Imm<4> imm4, Imm<3> imm3, Reg d, Imm<8> imm8);
