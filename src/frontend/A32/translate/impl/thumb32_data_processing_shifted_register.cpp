17bdb54d303fc4f4a36862423002c7edcdfbb0b0
MerryMage <MerryMage@users.noreply.github.com>
2021-03-06 19:44:38 +0000

    thumb32: Implement CMP (reg)

--- src/frontend/A32/translate/impl/thumb32_data_processing_shifted_register.cpp
+++ src/frontend/A32/translate/impl/thumb32_data_processing_shifted_register.cpp
@@ -238,4 +238,19 @@ bool ThumbTranslatorVisitor::thumb32_SBC_reg(bool S, Reg n, Imm<3> imm3, Reg d, 
     return true;
 }
 
+bool ThumbTranslatorVisitor::thumb32_CMP_reg(Reg n, Imm<3> imm3, Imm<2> imm2, ShiftType type, Reg m) {
+    if (n == Reg::PC || m == Reg::PC) {
+        return UnpredictableInstruction();
+    }
+
+    const auto shifted = EmitImmShift(ir.GetRegister(m), type, imm3, imm2, ir.GetCFlag());
+    const auto result = ir.SubWithCarry(ir.GetRegister(n), shifted.result, ir.Imm1(1));
+
+    ir.SetNFlag(ir.MostSignificantBit(result.result));
+    ir.SetZFlag(ir.IsZero(result.result));
+    ir.SetCFlag(result.carry);
+    ir.SetVFlag(result.overflow);
+    return true;
+}
+
 } // namespace Dynarmic::A32
