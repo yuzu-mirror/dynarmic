65ed1e7054683b7e3d4e6527c9c864a94d4a1499
Merry <git@mary.rs>
2022-04-23 19:37:17 +0100

    mcl/assert: Flush stderr

--- externals/mcl/src/assert.cpp
+++ externals/mcl/src/assert.cpp
@@ -14,6 +14,7 @@ namespace mcl::detail {
 [[noreturn]] void assert_terminate_impl(fmt::string_view msg, fmt::format_args args) {
     fmt::print(stderr, "assertion failed: ");
     fmt::vprint(stderr, msg, args);
+    std::fflush(stderr);
     std::terminate();
 }
 
