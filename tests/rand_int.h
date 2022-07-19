ecacb6cdc650b6964fc7f7b61bf33728409cc13c
Merry <git@mary.rs>
2022-07-19 20:47:26 +0100

    tests/rand_int: Expose PRNG

--- tests/rand_int.h
+++ tests/rand_int.h
@@ -8,14 +8,20 @@
 #include <random>
 #include <type_traits>
 
+namespace detail {
+inline std::mt19937 g_rand_int_generator = [] {
+    std::random_device rd;
+    std::mt19937 mt{rd()};
+    return mt;
+}();
+}  // namespace detail
+
 template<typename T>
 T RandInt(T min, T max) {
     static_assert(std::is_integral_v<T>, "T must be an integral type.");
     static_assert(!std::is_same_v<T, signed char> && !std::is_same_v<T, unsigned char>,
                   "Using char with uniform_int_distribution is undefined behavior.");
 
-    static std::random_device rd;
-    static std::mt19937 mt(rd());
     std::uniform_int_distribution<T> rand(min, max);
-    return rand(mt);
+    return rand(detail::g_rand_int_generator);
 }
