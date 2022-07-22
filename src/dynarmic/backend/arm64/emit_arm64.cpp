92026a456a79aac9891e3378f1cacb02f1efb7d3
Merry <git@mary.rs>
2022-07-22 20:01:13 +0100

    backend/arm64: Implement GetUpperFromOp

--- src/dynarmic/backend/arm64/emit_arm64.cpp
+++ src/dynarmic/backend/arm64/emit_arm64.cpp
@@ -87,11 +87,9 @@ void EmitIR<IR::Opcode::GetNZFromOp>(oaknut::CodeGenerator& code, EmitContext& c
 }
 
 template<>
-void EmitIR<IR::Opcode::GetUpperFromOp>(oaknut::CodeGenerator& code, EmitContext& ctx, IR::Inst* inst) {
-    (void)code;
-    (void)ctx;
-    (void)inst;
-    ASSERT_FALSE("Unimplemented");
+void EmitIR<IR::Opcode::GetUpperFromOp>(oaknut::CodeGenerator&, EmitContext& ctx, IR::Inst* inst) {
+    [[maybe_unused]] auto args = ctx.reg_alloc.GetArgumentInfo(inst);
+    ASSERT(ctx.reg_alloc.IsValueLive(inst));
 }
 
 template<>
