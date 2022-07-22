ec3c59759169edfc7a5796fd5c71204c8eace48d
Merry <git@mary.rs>
2022-07-23 00:00:59 +0100

    backend/arm64: Implement LeastSignificantByte

--- src/dynarmic/backend/arm64/emit_arm64_data_processing.cpp
+++ src/dynarmic/backend/arm64/emit_arm64_data_processing.cpp
@@ -95,10 +95,13 @@ void EmitIR<IR::Opcode::LeastSignificantHalf>(oaknut::CodeGenerator& code, EmitC
 
 template<>
 void EmitIR<IR::Opcode::LeastSignificantByte>(oaknut::CodeGenerator& code, EmitContext& ctx, IR::Inst* inst) {
-    (void)code;
-    (void)ctx;
-    (void)inst;
-    ASSERT_FALSE("Unimplemented");
+    auto args = ctx.reg_alloc.GetArgumentInfo(inst);
+
+    auto Wresult = ctx.reg_alloc.WriteW(inst);
+    auto Woperand = ctx.reg_alloc.ReadW(args[0]);
+    RegAlloc::Realize(Wresult, Woperand);
+
+    code.UXTB(Wresult, Woperand);  // TODO: Zext elimination
 }
 
 template<>
