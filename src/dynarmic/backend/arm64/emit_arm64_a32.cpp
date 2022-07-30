33b3376fb19dfe03858abe0550da7e621d806df4
Merry <git@mary.rs>
2022-07-30 19:10:35 +0100

    emit_arm64_a32: Implement A32SetCpsrNZCVRaw, A32SetCpsrNZCVQ

--- src/dynarmic/backend/arm64/emit_arm64_a32.cpp
+++ src/dynarmic/backend/arm64/emit_arm64_a32.cpp
@@ -274,18 +274,24 @@ void EmitIR<IR::Opcode::A32SetCpsrNZCV>(oaknut::CodeGenerator& code, EmitContext
 
 template<>
 void EmitIR<IR::Opcode::A32SetCpsrNZCVRaw>(oaknut::CodeGenerator& code, EmitContext& ctx, IR::Inst* inst) {
-    (void)code;
-    (void)ctx;
-    (void)inst;
-    ASSERT_FALSE("Unimplemented");
+    auto args = ctx.reg_alloc.GetArgumentInfo(inst);
+    auto Wnzcv = ctx.reg_alloc.ReadW(args[0]);
+    RegAlloc::Realize(Wnzcv);
+
+    code.STR(Wnzcv, Xstate, offsetof(A32JitState, cpsr_nzcv));
 }
 
 template<>
 void EmitIR<IR::Opcode::A32SetCpsrNZCVQ>(oaknut::CodeGenerator& code, EmitContext& ctx, IR::Inst* inst) {
-    (void)code;
-    (void)ctx;
-    (void)inst;
-    ASSERT_FALSE("Unimplemented");
+    auto args = ctx.reg_alloc.GetArgumentInfo(inst);
+    auto Wnzcv = ctx.reg_alloc.ReadW(args[0]);
+    RegAlloc::Realize(Wnzcv);
+
+    static_assert(offsetof(A32JitState, cpsr_nzcv) + sizeof(u32) == offsetof(A32JitState, cpsr_q));
+
+    code.AND(Wscratch0, Wnzcv, 0xf000'0000);
+    code.AND(Wscratch1, Wnzcv, 0x0800'0000);
+    code.STP(Wscratch0, Wscratch1, Xstate, offsetof(A32JitState, cpsr_nzcv));
 }
 
 template<>
