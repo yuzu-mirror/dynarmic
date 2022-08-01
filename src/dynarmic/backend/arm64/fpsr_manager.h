60a119da6ac188dc9f4df77163cb327b4c85b25e
Merry <git@mary.rs>
2022-08-02 00:33:59 +0100

    backend: Implement FpsrManager

---
+++ src/dynarmic/backend/arm64/fpsr_manager.h
@@ -0,0 +1,33 @@
+/* This file is part of the dynarmic project.
+ * Copyright (c) 2022 MerryMage
+ * SPDX-License-Identifier: 0BSD
+ */
+
+#pragma once
+
+#include <mcl/stdint.hpp>
+
+namespace oaknut {
+struct PointerCodeGeneratorPolicy;
+template<typename>
+class BasicCodeGenerator;
+using CodeGenerator = BasicCodeGenerator<PointerCodeGeneratorPolicy>;
+}  // namespace oaknut
+
+namespace Dynarmic::Backend::Arm64 {
+
+class FpsrManager {
+public:
+    explicit FpsrManager(oaknut::CodeGenerator& code, size_t state_fpsr_offset);
+
+    void Spill();
+    void Load();
+    void Overwrite() { fpsr_loaded = false; }
+
+private:
+    oaknut::CodeGenerator& code;
+    size_t state_fpsr_offset;
+    bool fpsr_loaded = false;
+};
+
+}  // namespace Dynarmic::Backend::Arm64
